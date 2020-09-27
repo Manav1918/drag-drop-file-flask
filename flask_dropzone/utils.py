e8e1ba729c5a88f915dbaee45f9e584d3fedd894
Pawan Kumar Prachi <41276382+Manav1918@users.noreply.github.com>
2020-09-27 17:03:52 +0530

    Add files via upload

---
+++ flask_dropzone/utils.py
@@ -0,0 +1,28 @@
+"""
+    flask_dropzone.utils
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    :author: Grey Li <withlihui@gmail.com>
+    :copyright: (c) 2017 by Grey Li.
+    :license: MIT, see LICENSE for more details.
+"""
+import os
+import uuid
+
+from flask import url_for
+
+
+def get_url(endpoint_or_url, **kwargs):
+    if endpoint_or_url == '':
+        return
+    if endpoint_or_url.startswith(('https://', 'http://', '/')):
+        return endpoint_or_url
+    else:
+        return url_for(endpoint_or_url, **kwargs)
+
+
+#: generate a random filename, replacement for werkzeug.secure_filename
+def random_filename(old_filename):
+    ext = os.path.splitext(old_filename)[1]
+    new_filename = uuid.uuid4().hex + ext
+    return new_filename
